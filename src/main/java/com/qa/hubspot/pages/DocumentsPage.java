41dcf95e3181caeec85baf644cfeb32d3f47d340
Ramesh <50035558@acsind.com>
2020-11-25 00:31:50 +0530

    addressed commnets in DocumenttsPage

--- src/main/java/com/qa/hubspot/pages/DocumentsPage.java
+++ src/main/java/com/qa/hubspot/pages/DocumentsPage.java
@@ -1,7 +1,9 @@
 package com.qa.hubspot.pages;
-
 import com.qa.hubspot.base.BasePage;
 
 public class DocumentsPage extends BasePage{
+	public DocumentsPage(){
+		System.out.println("DocumentsPage Constructor");
+	}
 
 }
